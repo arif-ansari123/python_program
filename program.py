01aca2e36ac863cf0fceee7fae5c3a1e7c0c1d08
arif-ansari123 <132267166+arif-ansari123@users.noreply.github.com>
2023-05-04 18:59:21 +0530

    generate a random number

--- program.py
+++ program.py
@@ -88,3 +88,13 @@ print('The value of y after swapping: {}'.format(y))
 
 #-------------------------------------------------------------------------------------
 
+# Program to generate a random number between 0 and 9
+
+# importing the random module
+import random
+
+print(random.randint(0,9))
+
+#---------------------------------------------------------------------------------------
+
+
