12a3775e8b4a474e2c6bb99e2ad3fdf5986bca08
arif-ansari123 <132267166+arif-ansari123@users.noreply.github.com>
2023-05-05 20:59:02 +0530

    convert temperature in celsius to fahrenheit

--- program.py
+++ program.py
@@ -109,3 +109,13 @@ print('%0.2f kilometers is equal to %0.2f miles' %(kilometers,miles))
 
 #------------------------------------------------------------------------------------------
 
+# Python Program to convert temperature in celsius to fahrenheit
+
+# change this value for a different result
+celsius = 37.5
+
+# calculate fahrenheit
+fahrenheit = (celsius * 1.8) + 32
+print('%0.1f degree Celsius is equal to %0.1f degree Fahrenheit' %(celsius,fahrenheit))
+
+#-------------------------------------------------------------------------------------
