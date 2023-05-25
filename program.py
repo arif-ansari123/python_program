7620c48547dfcf7b37af8ce35fb141468cc25f45
arif-ansari123 <132267166+arif-ansari123@users.noreply.github.com>
2023-05-25 22:40:06 +0530

    Update program.py

--- program.py
+++ program.py
@@ -809,3 +809,17 @@ for i in range(rows):
         print(j+1, end=" ")
     print("\n")
 #-----------------------------_------------------------------------------
+
+rows = int(input("Enter number of rows: "))
+
+ascii_value = 65
+
+for i in range(rows):
+    for j in range(i+1):
+        alphabet = chr(ascii_value)
+        print(alphabet, end=" ")
+    
+    ascii_value += 1
+    print("\n")
+#-----------------------------_------------------------------------------
+#-----------------------------_------------------------------------------
